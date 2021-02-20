bd4d9f1716cb76178f458a3aecdaf30184a46ca4
ArtemiiSelyankin <artemselyankin@gmail.com>
2021-02-21 02:23:07 +0300

    Delete BookmarkManager directory

--- BookmarkManager/GeneralOperations.cpp
+++
@@ -1,9 +0,0 @@
-#include "GeneralOperations.h"
-
-void moveWindowToCenterScreen(HWND hWnd, HWND hWndInsertAfter)
-{
-	RECT rect;
-
-	GetWindowRect(hWnd, &rect);
-	SetWindowPos(hWnd, hWndInsertAfter, SCREENX - ((rect.right - rect.left) / 2), SCREENY - ((rect.bottom - rect.top) / 2), NULL, NULL, SWP_NOSIZE);
-}
